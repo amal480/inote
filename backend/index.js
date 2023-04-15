933f786bd78597c291fccba4115abf11a2756fde
amal <amalmohan480@gmail.com>
2023-04-15 19:13:29 +0530

    3rd

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,21 @@ connecttomongo();
 const app = express()
 const port = 3000
 
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cors());
+app.use(express.json())
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello!')
 })
+
+
+//Available routes
+app.use('/api/auth',require('./routes/auth'))
+// app.use('/api/notes',require('./routes/notes'))
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
